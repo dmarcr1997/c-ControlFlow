14b03f66a493a7278c08388bfb2bba3c429f1c2b
dmarcr1997 <dmarcr1997@gmail.com>
2020-07-29 11:02:14 -0500

    finished exercise

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# c-ControlFlow
+﻿# c-ControlFlow
+
+###Write a program that calculates the sum of a collection of positive integers that
+are entered by the user from the keyboard. Your program should prompt the user
+to enter each integer followed by the return key, and to enter “−1” at the end
+of the list of integers to be added. Note that there is no need to store the list of
+integers: you can keep track of the sum as the user is entering the values.
+2. Modify your code so that the code terminates if the sum of integers entered up to
+that point exceeds 100.
+3. Modify your code so that, if the user has entered an incorrect integer, they may
+enter “−2” to reset the sum to zero and begin entering integers again.###
+
+
+# c-vect
+
+efine vectors x, y, and z, and matrices C and D, such that
+x = u − v,
+y = Au,
+z = Au − v,
+C = 4A − 3B,
+D = AB.
--- vect.cpp
+++ vect.cpp
@@ -3,29 +3,67 @@ using namespace std;
 int main() {
 	double u[3] = { 1.0, 2.0, 3.0 };
 	double v[3] = { 6.0, 5.0, 4.0 };
-	double A[3][3] = { {1.0, 5.0, 0.0},
-	{7.0, 1.0, 2.0},
-	{0.0, 0.0, 1.0} };
-	double B[3][3] = { {-2.0, 0.0, 1.0},
-	{1.0, 0.0, 0.0},
-	{4.0, 1.0, 0.0} };
+	double A[3][3] = {
+		{1.0, 5.0, 0.0},
+		{7.0, 1.0, 2.0},
+		{0.0, 0.0, 1.0}
+	};
+	double B[3][3] = {
+		{-2.0, 0.0, 1.0},
+		{1.0, 0.0, 0.0},
+		{4.0, 1.0, 0.0}
+	};
 
-	double w[3];
 	double x[3];
 	double y[3];
+	double z[3];
 	double C[3][3];
-	double num = 0;
+	double D[3][3];
+	double dotSum = 0;
+	for (int i = 0; i < 3; i++) {
+		x[i] = u[i] - v[i];
+		cout << x[i] << endl;
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			dotSum += A[i][j] * u[j];
+		}
+		y[i] = dotSum;
+		dotSum = 0;
+		cout << y[i] << endl;
+	}
 	for (int i = 0; i < 3; i++)
 	{
-		w[i] = u[i] - 3.0 * v[i];
-		x[i] = u[i] - v[i];
-		for (int j = 0; j < 3; j++) {
-			j += (A[j][i] * u[i]);
-			C[i][j] = (4 * A[i][j]) - (3 * B[i][j]);
-		}
-		y[i] = j;
-		z[i] = j - v[i];
+		z[i] = y[i] - v[i];
+		cout << z[i] << endl;
 	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			C[i][j] = (4 * A[i][j]) - (3 * B[i][j]);
+			cout << C[i][j] << " ";
+		}
+		cout << endl;
+	}
+	int count = 0;
+	double  dotProd = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			for (int k = 0; k < 3; k++)
+			{
+				dotProd += A[i][k] * B[k][j];
+			}
+			D[i][j] = dotProd;
+			dotProd = 0;
+			cout << D[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
 
 	return 0;
 }
